19e11eb489398461e36aa2e8b23a4c28728f0937
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2026-02-22 21:53:42 +0100

    Update BMGcalc.py

--- BMGcalc.py
+++ BMGcalc.py
@@ -2,6 +2,6 @@ import os
 import trained_stage2_with_predictions_flexible as s2_train
 
 # Check if the Stage 2 artifacts exist
-if not os.path.exists("STAGE2_classificatuion/artifacts.joblib"):
+if not os.path.exists("/mount/src/bmgcalc/STAGE2_classificatuion/artifacts.joblib"):
     with st.spinner("Initializing models for the first time... this may take a minute."):
         s2_train.train_model() # This runs your training script on the server
